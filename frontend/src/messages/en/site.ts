25228e2bcbf97cae6d4b822f143da0e0b3be10cd
unlibra <63234154+unlibra@users.noreply.github.com>
2025-12-07 15:06:14 +0900

    Change site name

--- frontend/src/messages/en/site.ts
+++ frontend/src/messages/en/site.ts
@@ -2,14 +2,14 @@ import type { site as jaSite } from '../ja/site'
 import type { SameStructure } from '../type-utils'
 
 export const site: SameStructure<typeof jaSite> = {
-  name: '8px.app',
+  name: '8px app',
   author: 'unlibra',
   description:
-    'Craft your perfect colors and icons. 8px.app offers a free toolkit for web and app developers, including favicon generation, color palette creation, SVG optimization, and more. No account needed, start creating instantly.',
+    'Craft your perfect colors and icons. 8px app offers a free toolkit for web and app developers, including favicon generation, color palette creation, SVG optimization, and more. No account needed, start creating instantly.',
   heroDescription:
     'Your vision, your design.\nA comprehensive toolkit for web and app developers.\nCompletely free, entirely open source.',
   title: {
-    default: '8px.app - Developer Toolkit',
-    template: '%s | 8px.app',
+    default: '8px app - Developer Toolkit',
+    template: '%s | 8px app',
   },
 } as const
--- frontend/src/messages/ja/site.ts
+++ frontend/src/messages/ja/site.ts
@@ -1,12 +1,12 @@
 export const site = {
-  name: '8px.app',
+  name: '8px app',
   author: 'unlibra',
   description:
     'その色も、そのアイコンも、思い通りに。\nFavicon生成、カラーパレット作成、SVG最適化など、Web・アプリ開発者のための無料ツールセット。\nアカウント登録不要、すぐに使えます。',
   heroDescription:
     'その色も、そのアイコンも、思い通りに。\nWeb・アプリ開発者のためのツールセット。\nすべて無料、すべてオープンソース。',
   title: {
-    default: '8px.app - Developer Toolkit',
-    template: '%s | 8px.app',
+    default: '8px app - Developer Toolkit',
+    template: '%s | 8px app',
   },
 } as const
