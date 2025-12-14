9c8b67f35aab1c8ddf6df8dd89025a2cd184e653
unlibra <63234154+unlibra@users.noreply.github.com>
2025-12-14 20:33:58 +0900

    Update logo and icons

--- frontend/src/components/icons/logo-icon.tsx
+++ frontend/src/components/icons/logo-icon.tsx
@@ -1,16 +1,12 @@
 export function LogoIcon ({ className }: { className?: string }) {
   return (
-    <svg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' className={className}>
-      <rect x='1' y='1' width='6' height='6' rx='0.75' className='fill-[#5bc5dc] dark:fill-[#f0f0f0]' />
-      <rect x='9' y='1' width='6' height='6' rx='0.75' className='fill-[#3ba0be] dark:fill-[#CFCFCF]' />
-      <rect x='17' y='1' width='6' height='6' rx='0.75' className='fill-[#1a7ba0] dark:fill-[#999999]' />
-
-      <rect x='1' y='9' width='6' height='6' rx='0.75' className='fill-[#3ba0be] dark:fill-[#CFCFCF]' />
-      <rect x='9' y='9' width='6' height='6' rx='0.75' className='fill-[#1a7ba0] dark:fill-[#999999]' />
-      <rect x='17' y='9' width='6' height='6' rx='0.75' className='fill-[#f88c49] dark:fill-[#7A7A7A]' />
-
-      <rect x='1' y='17' width='6' height='6' rx='0.75' className='fill-[#1a7ba0] dark:fill-[#999999]' />
-      <rect x='9' y='17' width='6' height='6' rx='0.75' className='fill-[#f88c49] dark:fill-[#7A7A7A]' />
+    <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 62 62.105' className={className}>
+      <path d='m34 2.105 12 56q0 4-4 4H20q-4 0-4-4l12-56c1.664-2.783 4.475-2.83 6 0' className='fill-[#5bc5dc] dark:fill-[#cbcbcb]' />
+      <g transform='translate(1 2.55244)'>
+        <rect y='16' width='60' height='8' rx='4' className='fill-[#1a7ba0] dark:fill-[#f5f5f5]' />
+        <circle cx='5' cy='35' r='6' className='fill-[#f88c49] dark:fill-[#a1a1a1]' />
+        <circle cx='55' cy='35' r='6' className='fill-[#f88c49] dark:fill-[#a1a1a1]' />
+      </g>
     </svg>
   )
 }
